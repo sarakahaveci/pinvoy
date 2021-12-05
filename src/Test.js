311150035c7c384d44eafb53bd71132aa307da72
yamoonz <yamooonz@gmail.com>
2021-12-05 03:17:11 +0300

    first commit

--- src/Test.js
+++ src/Test.js
@@ -1,36 +1,32 @@
 import React from 'react';
+import { render } from 'react-dom';
 import { CsvToHtmlTable } from 'react-csv-to-table';
 import ReactFileReader from 'react-file-reader';
-import './App.css';
 
-const sampleData = `a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,y,z`;
+const sampleData = `
+Chrysler Imperial,14.7,8,440,230,3.23,5.345,17.42,0,0,3,4
+Fiat 128,32.4,4,78.7,66,4.08,2.2,19.47,1,1,4,1
+`;
 
 class App extends React.Component {
   state={
-    csvData: null,
-    setCsvFile: null
+    csvData: null
   };
 
   render(){
-      return <div className="container">
-      <h3>Pivony</h3>
-      <div className="field">
+      return <div>
         <ReactFileReader 
           multipleFiles={false}
           fileTypes={[".csv"]} 
         handleFiles={this.handleFiles}>
-        
           <button className='btn'>Upload</button>
-          <br/>
-          </ReactFileReader>
-        <CsvToHtmlTable   
+        </ReactFileReader>
+        <CsvToHtmlTable
           data={this.state.csvData || sampleData}
           csvDelimiter=","
           tableClassName="table table-striped table-hover"
         />
-      </div>;
-      </div>
-    }
+      </div>;}
 
   handleFiles = files => {
     var reader = new FileReader();
@@ -44,5 +40,4 @@ class App extends React.Component {
   }
 
 }
-
 export default App;
